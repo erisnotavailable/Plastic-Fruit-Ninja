b8b082dd70e32e81ddef49fc8375babc774993b8
Yuxuan Luo <luoyuxuan.carl@gmail.com>
2021-10-02 22:13:50 -0400

    continously launching & random velocity

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
     // Global Vars
-    private string nameInit = "W";
+    public string nameInit = "W";
+    private bool hasCut = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        FruitLauncher parentScript = GameObject.Find("FruitLauncher").GetComponent<FruitLauncher>(); 
+        FruitLauncher parentScript = GameObject.Find("FruitLauncher").GetComponent<FruitLauncher>();
         parentScript.numOfChildIncrement();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Detect key pressed, do nothing for keys other than WASD
+        if (!hasCut)
+        {
+            if (Input.GetKeyDown(nameInit.ToLower()))
+            {
+                hasCut = true;
+                Debug.Log("Correct");
+            }
+            else if (Input.GetKeyDown(KeyCode.W) ||
+              Input.GetKeyDown(KeyCode.A) ||
+              Input.GetKeyDown(KeyCode.S) ||
+              Input.GetKeyDown(KeyCode.D))
+            {
+                hasCut = true;
+                Debug.Log("Incorrect");
+            }
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         Debug.Log("Collide with border");
-        FruitLauncher parentScript = GameObject.Find("FruitLauncher").GetComponent<FruitLauncher>(); 
+        FruitLauncher parentScript = GameObject.Find("FruitLauncher").GetComponent<FruitLauncher>();
         parentScript.numOfChildDecrement();
         Object.Destroy(this.gameObject);
         Debug.Log("Destroyed cloned `Fruit`");
--- Assets/Scripts/FruitLauncher.cs
+++ Assets/Scripts/FruitLauncher.cs
@@ -7,24 +7,32 @@ public class FruitLauncher : MonoBehaviour
 {
     // Global Vars
     public GameObject fruitList;
-    public float launchVelocity_x = 400f;
-    public float launchVelocity_y = 400f;
+    public float velocityXMin = 300f;
+    public float velocityXMax = 700f;
+    public float velocityYMin = 300f;
+    public float velocityYMax = 700f;
     private int numOfChild = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("cloning...");
-        GameObject fruit = Instantiate(fruitList,
-            transform.position, transform.rotation);
-        fruit.GetComponent<Renderer>().enabled = true;
-        fruit.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-        fruit.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(launchVelocity_x, launchVelocity_y));
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (numOfChild == 0)
+        {
+            GameObject fruit = Instantiate(fruitList,
+                transform.position, transform.rotation);
+            fruit.GetComponent<Renderer>().enabled = true;
+            fruit.GetComponent<Fruit>().enabled = true;
+            fruit.GetComponent<Fruit>().nameInit = "w";
+            fruit.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            fruit.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(
+                Random.Range(velocityXMin, velocityXMax), Random.Range(velocityYMin, velocityYMax)));
+        }
 
     }
 
